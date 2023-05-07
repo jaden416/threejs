efbbf7c70d41a1ca6ab2a5ce3e4e883af9a9b294
jaden416 <jaden416@outlook.com>
2023-05-07 00:33:39 -0400

    done 09

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -11,8 +11,23 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+const geometry = new THREE.BufferGeometry() // create a brand new shape!
+
+const count = 30
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++){
+    positionsArray[i] = Math.random() - .5
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000, 
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
